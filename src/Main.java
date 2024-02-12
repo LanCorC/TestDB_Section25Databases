f54301f8c87945a7aaa3d1d5236aa98f6bb37c08
LanCorC <lanc3.corcega@gmail.com>
2024-02-12 17:21:26 +1300

    setting up

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\databases\\testjava.db");
+//            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\databases\\testjava.db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\lance\\Documents\\CodingJava\\TestDB_Section25Databases\\testjava.db");
+
         } catch(SQLException e) {
             System.out.println(e);
         }
