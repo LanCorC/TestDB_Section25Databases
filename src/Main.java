3527dba0ce513dbce849d25d319b48f1f9886800
LanCorC <lanc3.corcega@gmail.com>
2024-02-13 20:17:08 +1300

    Section 25.431(Databases: Creating Databases with JDBC in Java): completing the lecture

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        try {
+//        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\lance\\Documents\\CodingJava\\TestDB_Section25Databases\\testjava.db");
+//            Statement statement = conn.createStatement()) {
 //            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\databases\\testjava.db");
+
+        try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\lance\\Documents\\CodingJava\\TestDB_Section25Databases\\testjava.db");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+
+            statement.close();
+            conn.close();
+
 
         } catch(SQLException e) {
             System.out.println(e);
