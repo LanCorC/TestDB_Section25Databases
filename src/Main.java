aaabf62445a7bb9488a4d53139c8f94fe0099f26
LanCorC <lanc3.corcega@gmail.com>
2024-02-14 21:16:27 +1300

    Section 25.432(Databases: JDBC Insert, Update, Delete): completing the lecture

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,12 +8,29 @@ public class Main {
 
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\lance\\Documents\\CodingJava\\TestDB_Section25Databases\\testjava.db");
+//            conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts " +
+                    "(name TEXT, phone INTEGER, email TEXT)");
 
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES('Honk', 132435,'ponk@email.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES('Jane', 4829484,'jane@somewhere.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES('Fido', 9039,'dog@email.com')");
+//            statement.execute("UPDATE contacts SET phone=5566789 WHERE name='Jane'");
+//            statement.execute("DELETE FROM contacts WHERE name='Honk'");
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+            while(results.next()) {
+                System.out.println(results.getString("name") + " "
+                        + results.getInt("phone") + " "
+                        + results.getString("email"));
+            }
+            results.close();
             statement.close();
             conn.close();
-
 
         } catch(SQLException e) {
             System.out.println(e);
